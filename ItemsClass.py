849044bac3a62e743bd528f38f04496f48f38597
Nashr El Auliya <nashr@bu.edu>
2022-12-13 02:20:47 -0500

    Items class tweaked

--- ItemsClass.py
+++ ItemsClass.py
@@ -1,6 +1,7 @@
 #items class
 class Items:
-  def __init__(self, in_name, in_price, in_qty):
+  def __init__(self, in_name, in_price, in_qty, in_url):
     self.part_name = in_name
     self.price = in_price # total price
     self.qty = in_qty
+    self.url = in_url
